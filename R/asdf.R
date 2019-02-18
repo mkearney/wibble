d82fe6256ed2740e8eb22eb39df1ea88f0ceede2
Michael W. Kearney <kearneymw@missouri.edu>
2019-02-17 18:05:32 -0600

    Edit asdf

--- R/asdf.R
+++ R/asdf.R
@@ -1,7 +1,12 @@
 withatts <- function(x) {
-  atts <- attributes(x)
+  atts <- lapply(x, attributes)
+  atts <- atts[lengths(atts) > 0]
+  atts <- c(
+    attributes(x),
+    do.call(base::c, atts)
+  )
+  atts <- atts[grep("\\.?names$|^$", names(atts), invert = TRUE)]
   if (length(atts) == 0) return(list())
-  atts <- atts[!names(atts) %in% c("names", "class")]
   atts
 }
 
@@ -10,19 +15,27 @@ as_df <- function(.x) {
     txt <- h_text.xml_document(.x)
     .x <- xml2::as_list(.x)
     .x <- withatts(.x)
-    if (length(.x) == 0) return(data.frame())
+    if (length(.x) == 0 && length(txt) == 0) return(list())
     .x$text <- txt
-    .x
-  } )
+    as.list(.x)
+  })
   for (i in seq_along(.x)) {
     if (length(.x[[i]]) == 0) {
-      .x[[i]] <- list(node = i)
+      .x[[i]] <- list(.node_id = i)
     } else {
-      .x[[i]]$node <- i
+      .x[[i]]$.node_id <- i
     }
   }
-  .x <- .x[lengths(.x) > 0]
   .x <- dapr::lap(.x, tbltools::as_tbl_data)
+  vars <- unique(unlist(lapply(.x, names)))
+  for (i in seq_along(.x)) {
+    if (any(!vars %in% names(.x[[i]]))) {
+      navar <- vars[!vars %in% names(.x[[i]])]
+      for (j in navar) {
+        .x[[i]][[j]] <- NA
+      }
+    }
+  }
   .x <- tbltools::bind_rows_data(.x, fill = TRUE)
-  .x[, c("node", names(.x)[names(.x) != "node"])]
+  .x[, c(".node_id", names(.x)[names(.x) != ".node_id"])]
 }
